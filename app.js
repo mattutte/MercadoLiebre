ca23b568afaab058842248f6907fd7b38bd8cb93
Matías Rainero <ing.rainero@gmail.com>
2021-10-11 21:25:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static('public'));
+
+app.listen(3030, () => {
+    //console.log("Bien ahí");
+}); 
+
+app.get('/', (req, res) => {
+    const archivo = path.join(__dirname, './views/home.html')
+    res.sendFile(archivo);
+});
+
+app.get('/register', (req, res) => {
+    const archivo = path.join(__dirname, './views/register.html')
+    res.sendFile(archivo);
+});
+
+app.post('/register', (req, res) => {
+    req.body;
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    const archivo = path.join(__dirname, './views/login.html')
+    res.sendFile(archivo);
+});
+
+
